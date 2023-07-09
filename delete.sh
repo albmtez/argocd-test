0a97d590a3e0231880242827cf8ac0a3c3fd98d6
Alberto Martínez Pérez <alb.mtez@gmail.com>
2023-07-09 14:40:02 +0200

    add argo-cd application

---
+++ delete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl patch applications.argoproj.io/root -p '{"metadata":{"finalizers":[]}}' --type=merge
+kubectl patch applications.argoproj.io/apps-project-one -p '{"metadata":{"finalizers":[]}}' --type=merge
+kubectl patch applications.argoproj.io/apps-project-two -p '{"metadata":{"finalizers":[]}}' --type=merge
+kubectl patch applications.argoproj.io/projects -p '{"metadata":{"finalizers":[]}}' --type=merge
+kubectl patch applications.argoproj.io/prometheus-project-one -p '{"metadata":{"finalizers":[]}}' --type=merge
+kubectl patch applications.argoproj.io/jenkins-project-two -p '{"metadata":{"finalizers":[]}}' --type=merge
+kubectl patch applications.argoproj.io/argo-cd -p '{"metadata":{"finalizers":[]}}' --type=merge
